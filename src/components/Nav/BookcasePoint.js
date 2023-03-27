fb54609778c0c0f983452c35a1bf633790f6774d
kr_shin <cskr1013@gmail.com>
2023-03-27 15:12:15 +0900

    Nav 로그인, 페이지이동 기능 구현완료

---
+++ src/components/Nav/BookcasePoint.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const BookcasePoint = () => {
+  const [bookData, setBookData] = useState([]);
+
+  const isCheckBookcase = bookData.length >= 1;
+
+  useEffect(() => {
+    fetch('./data/keepBooksData.json')
+      .then(res => res.json())
+      .then(data => setBookData(data));
+  }, []);
+
+  return isCheckBookcase && <RedPoint />;
+};
+
+const RedPoint = styled.div`
+  content: '',
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background-color: red;
+  border: 1px solid #fff;
+`;
+
+export default BookcasePoint;
